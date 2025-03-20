25078df37cbab3411020ad8c0503ea40408a8a35
laquisha-ayee <rayelaquisha@gmail.com>
2025-03-20 12:25:17 -0700

    phase2

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+
 require('express-async-errors');
+
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -23,8 +25,7 @@ if (!isProduction) {
     // enable cors only in development
     app.use(cors());
   }
-  
-  // helmet helps set a variety of headers to better secure your app
+   // helmet helps set a variety of headers to better secure your app
   app.use(
     helmet.crossOriginResourcePolicy({
       policy: "cross-origin"
@@ -42,11 +43,39 @@ if (!isProduction) {
     })
   );
 
+app.use(routes);
 
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
 
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
 
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
-  app.use(routes);
-  
-  module.exports = app;
+module.exports = app;
 
