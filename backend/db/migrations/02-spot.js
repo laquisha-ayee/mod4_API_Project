efb5b68fa544ff27a1c73a582d5cf5ee5a5e426b
laquisha-ayee <rayelaquisha@gmail.com>
2025-04-28 17:46:30 -0700

    SpotSeederTest

--- backend/db/migrations/02-spot.js
+++ backend/db/migrations/02-spot.js
@@ -1,75 +1,88 @@
 'use strict';
 
-const { Model } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
-  class Spot extends Model {
-    static associate(models) {
-      Spot.belongsTo(models.User, { foreignKey: 'ownerId', onDelete: 'CASCADE' });
-    }
+/** @type {import('sequelize-cli').Migration} */
+
+
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'Spots',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        ownerId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: 'Users', key: 'id' },
+          onDelete: 'CASCADE'
+        },
+        address: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        city: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        state: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        country: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        lat: {
+          type: Sequelize.DECIMAL(10, 7),
+          allowNull: false
+        },
+        lng: {
+          type: Sequelize.DECIMAL(10, 7),
+          allowNull: false
+        },
+        name: {
+          type: Sequelize.STRING(50),
+          allowNull: false
+        },
+        description: {
+          type: Sequelize.TEXT,
+          allowNull: false
+        },
+        price: {
+          type: Sequelize.DECIMAL(10, 2),
+          allowNull: false
+        },
+        previewImage: {
+          type: Sequelize.STRING,
+          allowNull: true,
+          defaultValue: null
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        }
+      },
+      options );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Spots', options);
   }
-  
-  Spot.init(
-    {
-      ownerId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      address: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      city: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      state: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      country: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lat: {
-        type: DataTypes.DECIMAL(10, 7),
-        allowNull: false,
-      },
-      lng: {
-        type: DataTypes.DECIMAL(10, 7),
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-      },
-      previewImage: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-    },
-    {
-      sequelize,
-      modelName: 'Spot',
-    }
-  );
-
-  return Spot;
 };
