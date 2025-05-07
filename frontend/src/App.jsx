8064fc66fcda49a7f0abab382502127ff7e7bf52
laquisha-ayee <rayelaquisha@gmail.com>
2025-05-07 13:53:17 -0700

    phase3Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 // frontend/src/App.jsx
-
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage';
-import Navigation from './components/Navigation';
+import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import Navigation from './components/Navigation/Navigation';
 import * as sessionActions from './store/session';
 
 function Layout() {
