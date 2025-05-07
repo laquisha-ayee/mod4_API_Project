b9fcf9acbaf2ddea3c71cf56cf23e5a364a865e5
laquisha-ayee <rayelaquisha@gmail.com>
2025-05-07 15:51:47 -0700

    phase4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 // frontend/src/App.jsx
+
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage/SignupFormPage';
-import Navigation from './components/Navigation/Navigation';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navigation from "./components/Navigation/Navigation";
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -32,14 +31,6 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <h1>Welcome!</h1>
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />
       }
     ]
   }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,26 +6,23 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
-
+import { ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
-
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
     </Provider>
   </React.StrictMode>
 );
